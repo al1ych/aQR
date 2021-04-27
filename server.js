30a573b35b0c4dab9f3ce10dad4228eeb075bad8
timurnugaev <decter49pro@gmail.com>
2021-04-28 01:02:27 +0300

    test auto switching camera

--- server.js
+++ server.js
@@ -8,7 +8,30 @@ app.use(express.json({strict: false, limit: '128mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-let server = app.listen(port, () =>
-{
+let current_link = 'undefined';
+
+
+app.get('/test', (req, res) => {
+    res.end('hello');
+});
+
+app.post('/link_up', (rq, rs) => {
+    let q = rq.body;
+    let link = q.link;
+    console.log('req', q);
+
+    current_link = link;
+    console.log('cur link updated', current_link);
+
+    rs.end('success');
+});
+
+app.get('/link_down', (rq, rs) => {
+    console.log('cur link sent to client', current_link);
+    rs.end(current_link);
+});
+
+
+let server = app.listen(port, () => {
     console.log("Server started...");
 });
