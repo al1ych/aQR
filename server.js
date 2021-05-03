c25ac9f9fd456a2bc6f87ab959d5ea9c9dcbe1ba
timurnugaev <decter49pro@gmail.com>
2021-05-03 13:51:57 +0300

    iframe test

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
-// app.use(express.urlencoded({extended: true, limit: '128mb'}));
 app.use(express.json({strict: false, limit: '128mb'}));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,8 +19,6 @@ let server = app.listen(port, () => {
 const {Server} = require("socket.io");
 const io = new Server(server);
 let qrVisited = {};
-// let socket = require("socket.io");
-// let io = socket(server);
 io.sockets.on('connection', (socket) => {
     console.log('new connection:', socket.id);
     socket.on('link', link => {
@@ -33,27 +30,5 @@ io.sockets.on('connection', (socket) => {
             qrVisited[link] = true;
         }
         socket.broadcast.emit('link', current_link);
-        // socket.emit('link', current_link);
     });
 });
-
-// app.get('/test', (req, res) => {
-//     res.end('hello');
-// });
-//
-// app.post('/link_up', (rq, rs) => {
-//     let q = rq.body;
-//     let link = q.link;
-//     console.log('req', q);
-//
-//     current_link = link;
-//     console.log('cur link updated', current_link);
-//
-//     rs.end('success');
-// });
-//
-// app.get('/link_down', (rq, rs) => {
-//     console.log('cur link sent to client', current_link);
-//     rs.end(current_link);
-// });
-
